c2ef8b7e18da1016eebd9eca1c7b0b506f84a51e
BigNona657 <bignona657@github.com>
2026-07-27 03:14:35 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,13 +57,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
+      { url: "/icons/logo.png", type: "image/png" },
     ],
     apple: [
-      { url: "/icons/icon-152x152.png", sizes: "152x152", type: "image/png" },
-      { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/logo.png", type: "image/png" },
     ],
+    shortcut: "/icons/logo.png",
   },
 };
 
